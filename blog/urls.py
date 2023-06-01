39dd2a3de1c2881a6065a3503c93882aeb14ac26
superherojt <ademolathompson5@gmail.com>
2023-06-01 17:30:26 +0100

    added docstrings and comments to help the users navigate the project

--- blog/urls.py
+++ blog/urls.py
@@ -1,3 +1,7 @@
+"""
+To use the function-based view, uncomment lines 6 and 9 and comment lines 7 and 10
+"""
+
 from django.urls import path
 # from .views import list_view
 from .views import PostListView
